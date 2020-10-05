768317ec25c83729a9da847f2d67c650b1bf8aa6
jyunya takahashi <mickey.co.121@gmail.com>
2020-10-05 17:30:22 +0900

    viewのtemplatesを追加

---
+++ view/templates/head.php
@@ -0,0 +1,19 @@
+<!-- 情報ヘッダ -->
+
+<!-- 文字コードを指定 -->
+<meta charset="UTF-8">
+<!-- 表示方法の指定 -->
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<!-- IEを最新の状態で表示する設定 -->
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+
+<!-- Bootstrap設定 -->
+<!-- Bootstrap用CSS -->
+<link rel="stylesheet" href="//stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<!-- jquery -->
+<script src="//code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<!-- popper.js（ポップアップ表示用ライブラリ） -->
+<script src="//cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<!-- Bootstrap用JS -->
+<script src="//stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
