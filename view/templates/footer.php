c26bbe5557291678f5d0d73aa501ac352abb6599
jyunya takahashi <mickey.co.121@gmail.com>
2020-10-31 23:15:17 +0900

    フッター変更#30

--- view/templates/footer.php
+++ view/templates/footer.php
@@ -2,40 +2,19 @@
 <footer class="page-footer font-small teal pt-4 bg-light">
 
   <!-- Footer Text -->
-  <div class="container-fluid text-center text-md-left">
-
-    <!-- Grid row -->
+  <div class="container-fluid text-center">
     <div class="row">
-
-      <!-- Grid column -->
-      <div class="col-md-6 mt-md-0 mt-3">
-
-        <!-- Content -->
-        <h5 class="text-uppercase font-weight-bold">Footer text 1</h5>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita sapiente sint, nulla, nihil
-          repudiandae commodi voluptatibus corrupti animi sequi aliquid magnam debitis, maxime quam recusandae
-          harum esse fugiat. Itaque, culpa?</p>
-
+      <div class="col text-right">
+        <img src="./assets/img/img_glasses2.png" width="100" height="100" alt="profile_img">
       </div>
-      <!-- Grid column -->
 
-      <hr class="clearfix w-100 d-md-none pb-3">
-
-      <!-- Grid column -->
-      <div class="col-md-6 mb-md-0 mb-3">
-
+      <div class="col text-left">
         <!-- Content -->
-        <h5 class="text-uppercase font-weight-bold">Footer text 2</h5>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio deserunt fuga perferendis modi earum
-          commodi aperiam temporibus quod nulla nesciunt aliquid debitis ullam omnis quos ipsam, aspernatur id
-          excepturi hic.</p>
-
+        <p>Created By</p>
+        <h4 class="text-uppercase font-weight-bold mb-3">Junya_Takahashi</h4>
+        <p>WEBengineerを目指して上京。ときどき旅行を楽しみながらPHPを学習中...</p>
       </div>
-      <!-- Grid column -->
-
     </div>
-    <!-- Grid row -->
-
   </div>
   <!-- Footer Text -->
 
